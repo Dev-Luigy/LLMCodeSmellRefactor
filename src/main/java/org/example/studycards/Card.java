4b1d2fddc8df43622770d315fa60bf1f278fdb57
Dev-Luigy <luigygabriel18a@gmail.com>
2025-01-10 10:59:47 -0300

    refactor: Solved DataClass in Card File.

--- src/main/java/org/example/studycards/Card.java
+++ src/main/java/org/example/studycards/Card.java
@@ -26,7 +26,20 @@ public class Card {
     }
 
     public void edit(String question, String answer) {
-        setQuestion(question);
-        setAnswer(answer);
+        this.question = question;
+        this.answer = answer;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Card other = (Card) obj;
+        return question.equals(other.question) && answer.equals(other.answer);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * question.hashCode() + answer.hashCode();
     }
 }
