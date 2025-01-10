25f9983fa0f0e905b4a6384d7b7248d79907457d
waniasantos <waniakellys@gmail.com>
2025-01-10 18:31:22 -0300

    refactor: Solved NCSS in LeitnerSystem File.

--- src/main/java/org/example/studycards/LeitnerSystem.java
+++ src/main/java/org/example/studycards/LeitnerSystem.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class LeitnerSystem extends StudyMethod{
-    List<Box> boxes = null;
+public class LeitnerSystem extends StudyMethod {
+    private List<Box> boxes = null;
+
     public LeitnerSystem(String methodName) {
         super(methodName);
-        boxes = new ArrayList<>(Arrays.asList(new Box(), new Box(), new Box(), new Box(), new Box()));
+        initializeBoxes();
+    }
+
+    private void initializeBoxes() {
+        boxes = new ArrayList<>(Arrays.asList(
+                new Box(), new Box(), new Box(), new Box(), new Box()
+        ));
     }
 
     @Override
@@ -22,87 +29,102 @@ public class LeitnerSystem extends StudyMethod{
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder response = new StringBuilder();
-        int index = 0;
-        for(Box box : boxes){
-            response.append("Box ").append(index).append(": ").append(box.toString()).append("\n");
-            index++;
-        }
+        buildBoxesString(response);
         return response.toString();
     }
 
-    public void clearBoxes(){
+    private void buildBoxesString(StringBuilder response) {
+        for (int i = 0; i < boxes.size(); i++) {
+            response.append("Box ")
+                    .append(i)
+                    .append(": ")
+                    .append(boxes.get(i).toString())
+                    .append("\n");
+        }
+    }
+
+    public void clearBoxes() {
         boxes.clear();
-        boxes = new ArrayList<>(Arrays.asList(new Box(), new Box(), new Box(), new Box(), new Box()));
+        initializeBoxes();
     }
 
     public List<Box> getBoxes() {
         return boxes;
     }
 
-    public String getRandomCard(List<Box> otherBoxes){
-        if(otherBoxes == null){
+    public String getRandomCard(List<Box> otherBoxes) {
+        if (otherBoxes == null || otherBoxes.isEmpty()) {
             return null;
         }
-        if(otherBoxes.isEmpty()){
-            return null;
-        }
-        Box allBoxes = new Box();
-        for(Box box : otherBoxes){
-            allBoxes.addCards(box.getCards());
-        }
-        Integer randomCard = allBoxes.getRandomCard();
-        if(randomCard == null){
+
+        Integer randomCardId = getRandomCardFromBoxes(otherBoxes);
+        if (randomCardId == null) {
             return "No card found";
         }
+
+        return formatCardResponse(randomCardId);
+    }
+
+    private Integer getRandomCardFromBoxes(List<Box> otherBoxes) {
+        Box allBoxes = new Box();
+        for (Box box : otherBoxes) {
+            allBoxes.addCards(box.getCards());
+        }
+        return allBoxes.getRandomCard();
+    }
+
+    private String formatCardResponse(Integer cardId) {
         CardManager manager = CardManager.getCardManager();
-        Card card = manager.getCard(randomCard);
-        String response = "["+ randomCard + "] ";
-        response += "The random question was: " + card.getQuestion() + " | ";
-        response += "The answer is: " + card.getAnswer();
-        return  response;
+        Card card = manager.getCard(cardId);
+        return String.format("[%d] The random question was: %s | The answer is: %s",
+                cardId, card.getQuestion(), card.getAnswer());
     }
 
     public void addCardToBox(Integer id, Integer boxId) {
-        this.boxes.get(boxId).addCard(id);
+        boxes.get(boxId).addCard(id);
     }
 
     public void removeCardFromBox(Integer id, Integer boxId) {
-        this.boxes.get(boxId).removeCard(id);
+        boxes.get(boxId).removeCard(id);
     }
 
-    public Card takeCardFromBox(Integer boxId){
+    public Card takeCardFromBox(Integer boxId) {
         Integer cardId = boxes.get(boxId).getRandomCard();
         return this.cardManager.getCard(cardId);
     }
 
     public void boxIdValidation(Integer boxId) throws Exception {
-        if(boxId == null || boxId > (boxes.size()-1) || boxId <= 0){
+        if (isInvalidBoxId(boxId)) {
             throw new Exception("Invalid box ID");
         }
     }
 
-    public void upgradeCard(Integer cardId, Integer boxId) throws Exception {
-        boxIdValidation(boxId);
+    private boolean isInvalidBoxId(Integer boxId) {
+        return boxId == null || boxId > (boxes.size() - 1) || boxId <= 0;
+    }
 
-        Box refBox = boxes.get(boxId);
-        if(refBox.hasCard(cardId)){
-            throw new Exception("No card Found");
-        }
-        refBox.removeCard(cardId);
-        boxes.get(Math.min(boxId + 1, 4)).addCard(cardId);
+    public void upgradeCard(Integer cardId, Integer boxId) throws Exception {
+        moveCard(cardId, boxId, true);
     }
 
     public void downgradeCard(Integer cardId, Integer boxId) throws Exception {
-        boxIdValidation(boxId);
-
-        Box refBox = boxes.get(boxId);
-        if(refBox.hasCard(cardId)){
-            throw new Exception("No card Found");
-        }
-        refBox.removeCard(cardId);
-        boxes.get(Math.max(boxId - 1, 0)).addCard(cardId);
+        moveCard(cardId, boxId, false);
     }
 
+    private void moveCard(Integer cardId, Integer boxId, boolean isUpgrade) throws Exception {
+        boxIdValidation(boxId);
+        Box refBox = boxes.get(boxId);
+
+        if (refBox.hasCard(cardId)) {
+            throw new Exception("No card Found");
+        }
+
+        refBox.removeCard(cardId);
+        int newBoxId = isUpgrade ?
+                Math.min(boxId + 1, 4) :
+                Math.max(boxId - 1, 0);
+        boxes.get(newBoxId).addCard(cardId);
+    }
 }
